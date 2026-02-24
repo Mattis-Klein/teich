13b2e79dd33a6b2938476f256669f81849c6a207
Mattis K. <simchapoems18@gmail.com>
2026-02-24 13:42:24 -0500

    Tuesday Update

--- run_app.py
+++ run_app.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+
 from app.app import run
 
+
 if __name__ == "__main__":
-    # This script expects to live in the project root (sukkah_teich_sample)
+    # This script expects to live in the project root
     run(Path(__file__).resolve().parent)
